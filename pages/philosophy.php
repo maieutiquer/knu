a7d65803aba15528e8c9cb84b979994770b48004
gloria-konsept <gloria@konsept.nu>
2014-05-26 09:50:51 +0000

    philosophy.php j'ai mis en gras les entités de konsept (est-ce que ça fait aussi sur les autres pages philosophie?)

--- pages/philosophy.php
+++ pages/philosophy.php
@@ -5,9 +5,9 @@
     <div class="konseptnu-desc">
         <h1>konsept.nu</h1>
         <p>
-            konsept.nu regroupe plusieurs entités qui défendent les mêmes valeurs humaines.<br>
-            Ainsi, l’agence de communication konsept, l’association active dans les ressources humaines, human konsept 
-            et le groupe de professionnels, le kollectif, participent au soutien de l’association humanitaire konsept of Charity 
+            <strong>konsept.nu</strong> regroupe plusieurs entités qui défendent les mêmes valeurs humaines.<br>
+            Ainsi, l’agence de communication <strong>konsept</strong>, l’association active dans les ressources humaines, <strong>human konsept</strong>
+            et le groupe de professionnels, le <strong>kollectif</strong>, participent au soutien de l’association humanitaire <strong>konsept of Charity</strong>
             afin de permettre à celle-ci de disposer de fonds pour soutenir des projets humanitaires ou sociaux, en Suisse et à l’étranger.<br>
             Pourquoi «&nbsp;.nu&nbsp;» ?<br>
             Un simple clin d’œil phonétique : en anglais, «&nbsp;nu&nbsp;» se prononce comme «&nbsp;new&nbsp;», 
@@ -18,8 +18,8 @@
     <div class="charity-percentage-desc">
         <h1>10&thinsp;% d’honoraires reversés</h1>
         <p>
-            L’altruisme et le sens de la responsabilité qui animent konsept.nu se concrétisent notamment
-            par la rétrocession de 10&thinsp;% des honoraires générés par konsept sàrl et par human konsept.
+            L’altruisme et le sens de la responsabilité qui animent <strong>konsept.nu</strong> se concrétisent notamment
+            par la rétrocession de 10&thinsp;% des honoraires générés par <strong>konsept</strong> sàrl et par <strong>human konsept</strong>.
             Ainsi, pour chaque mandat réalisé, nous renonçons à une partie de nos revenus afin de les
             mettre à disposition pour aider autrui.
     </div>
@@ -31,9 +31,9 @@
         <h1>100&#8239;% don - 0&#8239;%&nbsp;frais</h1>
         <p>
             La confiance doit être la base de toute relation si l’on souhaite que celle-ci puisse s’inscrire dans le temps. 
-            Ainsi, l’association humanitaire konsept of Charity est basée sur notre philosophie unique «&nbsp;100% don – 0% frais&nbsp;». 
+            Ainsi, l’association humanitaire <strong>konsept of Charity</strong> est basée sur notre philosophie unique «&nbsp;100% don – 0% frais&nbsp;». 
             Concrètement, cela signifie que chaque franc versé est converti en aide, 
-            tous les frais de fonctionnement sont pris en charge par l’agence konsept sàrl.
+            tous les frais de fonctionnement sont pris en charge par l’agence <strong>konsept</strong> sàrl.
     </div>
 </article>
 </section>
