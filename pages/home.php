baf95bcd46186f10decbfc04e1c79ff49fd92242
daniel rodriguez <maieutiquer@gmail.com>
2014-05-28 10:55:17 +0300

    better code indentation on homepage

--- pages/home.php
+++ pages/home.php
@@ -3,12 +3,13 @@
   <ul class="homepage-links-holder">
     <li><a class="agency-link" href="agence"><span>agence</span></a>
     <li><a class="ikon-link" href="prestations"><span>ikon</span></a>
-    <li><ul class="multiple-links-holder">
-    <li><a class="human-konsept-link" href="human" target="_blank"><span>human konsept</span></a>
-    <li><a class="kofc-link" href="kofc" target="_blank"><span>konsept of&nbsp;Charity</span></a>
-    <li><a class="philosophy-link" href="philosophie"><span>philosophie</span></a>
-    <li><a class="shopk2-link" href="k2" target="_blank"><span>shop k²</span></a>
-    </ul>
+    <li>
+      <ul class="multiple-links-holder">
+        <li><a class="human-konsept-link" href="human" target="_blank"><span>human konsept</span></a>
+        <li><a class="kofc-link" href="kofc" target="_blank"><span>konsept of&nbsp;Charity</span></a>
+        <li><a class="philosophy-link" href="philosophie"><span>philosophie</span></a>
+        <li><a class="shopk2-link" href="k2" target="_blank"><span>shop k²</span></a>
+      </ul>
     <li><a class="welcome-link" href="agence"><span>bienvenue</span></a>
     <li><a class="services-link" href="prestations"><span>prestations</span></a>
     <li><a class="team-photo-link" href="equipe"><span>kollectif</span></a>
