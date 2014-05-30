a7b53313791730107b0b05202785409578e155f3
daniel rodriguez <maieutiquer@gmail.com>
2014-05-31 00:06:54 +0300

    refactor site titles, description and inline scripts

--- helper.php
+++ helper.php
@@ -5,12 +5,14 @@ const pages_dir = "pages";
 // TODO: separate css per page
 
 // declare page title and description
+$site_title = 'konsept';
+$page_title = null;
 $page_title_full = null;
-$page_desc_full = null;
+$page_desc = null;
 $page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
 
 $scripts = '';
-$inlineScripts = '';
+$inlineScripts = '<script>';
 $includeJQuery = '
 <!--[if lt IE 9]>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
@@ -18,9 +20,6 @@ $includeJQuery = '
 <!--[if gte IE 9]><!-->
 <script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
 <!--<![endif]-->';
-$includeZepto = '
-<script src="js/zepto.min.js"></script>
-';
 $includePopup = '
 <script src="js/jquery.colorbox-min.js"></script>
 <script src="js/popup.js"></script>';
@@ -39,19 +38,19 @@ switch($page) {
     $included_page = pages_dir."/agency.php";
     $nav_agency_class = " active";
     $agency_submenu = " opened";
+    $page_title = 'presentation';
     break;
   case "references":
   case "références":
     $included_page = pages_dir."/references.php";
     $nav_agency_class = " active";
     $nav_references_class = " active";
+    $page_title = 'références';
     break;
   case "prestations":
   case "services":
     $included_page = pages_dir."/services.php";
     $nav_services_class = " active";
-    $inlineScripts.= '
-<script>';
     $inlineScripts.= '
 document.getElementById("inline-popups").style.display = "none";';
     $scripts.= $includeJQuery;
@@ -62,6 +61,7 @@ document.getElementById("inline-popups").style.display = "none";';
   case "team":
     $included_page = pages_dir."/team.php";
     $nav_team_class = " active";
+    $page_title = 'équipe';
     break;
   case "philosophie":
   case "philosophy":
@@ -74,17 +74,21 @@ document.getElementById("inline-popups").style.display = "none";';
   default:
     $included_page = pages_dir."/home.php";
     $page_title_full = "agence de communication à Lausanne - konsept";
-    $page_desc_full = "Soutien administratif, expertise et conseil dans le domaine de l’habitat.";
     $inlineScripts = null;
+    $page_desc = 'Soutien administratif, expertise et conseil dans le domaine de l’habitat.';
     break;
 }
 
 $page_title_separator = " | ";
+if (!$page_title) {
+  $page_title = $page;
+}
 if (!$page_title_full) {
-  $page_title_full = $page.$page_title_separator."konsept - agence de communication à Lausanne";
+  $page_title_full = $page_title.$page_title_separator.$site_title;
 }
 
-if ($inlineScripts != null) {
+// check for inline scripts and add them to scripts declaration
+if ($inlineScripts != '<script>') {
   $inlineScripts.= '
 </script>';
   $scripts.= $inlineScripts;
--- tmpl/head.php
+++ tmpl/head.php
@@ -2,8 +2,8 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?=$page_title_full; ?></title>
-<?php if (isset($page_desc_full)) : ?>
-<meta name="description" content="<?=$page_desc_full; ?>">
+<?php if (isset($page_desc)) : ?>
+<meta name="description" content="<?=$page_desc; ?>">
 <?php endif; ?>
 <link rel="icon" href="favicon.png">
 <link rel="stylesheet" href="/min/b=knu/css&amp;f=normalize.css,style.css,colorbox.css,menu.css,footer.css,agency.css,references.css,services.css,team.css,philosophy.css">
