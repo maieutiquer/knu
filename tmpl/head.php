69eab82324131c93e4d045b58565cf39d882e62c
daniel rodriguez <maieutiquer@gmail.com>
2014-05-31 16:28:37 +0300

    move header styles to header.css

--- tmpl/head.php
+++ tmpl/head.php
@@ -6,4 +6,4 @@
 <meta name="description" content="<?=$page_desc; ?>">
 <?php endif; ?>
 <link rel="icon" href="favicon.png">
-<link rel="stylesheet" href="/min/b=knu/css&amp;f=normalize.css,style.css,colorbox.css,menu.css,footer.css,home.css,agency.css,references.css,services.css,team.css,philosophy.css">
+<link rel="stylesheet" href="/min/b=knu/css&amp;f=normalize.css,style.css,colorbox.css,header.css,menu.css,footer.css,home.css,agency.css,references.css,services.css,team.css,philosophy.css">
