cbf8458b12114b8cc880d95e4ff196d98580f18e
daniel rodriguez <maieutiquer@gmail.com>
2014-06-05 15:55:44 +0300

    make better 404 page

--- pages/not-found.php
+++ pages/not-found.php
@@ -1,6 +1,7 @@
 <?php defined('kexec') or die('Restricted access'); ?>
-<div class="not-found-container">
-  <p>Oops ! Cette page est introuvable.
+<div class="not-found">
+  <p>Oops ! La page /<?=$page?> n'existe pas.
+  <p><a href="./" rel="nofollow" class="goto-home-404">Retour vers la page d'accueil.</a>
 </div>
 
 <footer id="footer" role="contentinfo">
