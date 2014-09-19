5e1ce5b52bfb0f47e7d674437bd3ab311cdeebb2
gloria-konsept <gloria@konsept.nu>
2014-09-19 12:31:09 +0000

    media.php insertion des images

--- pages/media.php
+++ pages/media.php
@@ -45,4 +45,54 @@
   <a href="images/media/visuels-pp_media-01.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs01.jpg" alt="media-holder"></a>
 </div>
 
+<div class="media-element">
+  <a href="images/media/visuels-pp_media-02.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs02.jpg" alt="media-holder"></a>
+</div>
+
+<div class="media-element">
+  <a href="images/media/visuels-pp_media-03.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs03.jpg" alt="media-holder"></a>
+</div>
+
+<div class="media-element">
+  <a href="images/media/visuels-pp_media-04.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs04.jpg" alt="media-holder"></a>
+</div>
+
+<div class="media-element">
+  <a href="images/media/visuels-pp_media-05.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs05.jpg" alt="media-holder"></a>
+</div>
+
+<div class="media-element">
+  <a href="images/media/visuels-pp_media-06.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs06.jpg" alt="media-holder"></a>
+</div>
+
+<div class="media-element">
+  <a href="images/media/visuels-pp_media-07.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs07.jpg" alt="media-holder"></a>
+</div>
+
+<div class="media-element">
+  <a href="images/media/visuels-pp_media-08.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs08.jpg" alt="media-holder"></a>
+</div>
+
+<div class="media-element">
+  <a href="images/media/visuels-pp_media-09.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs09.jpg" alt="media-holder"></a>
+</div>
+
+<div class="media-element">
+  <a href="images/media/visuels-pp_media-010.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs010.jpg" alt="media-holder"></a>
+</div>
+
+<div class="media-element">
+  <a href="images/media/visuels-pp_media-011.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs011.jpg" alt="media-holder"></a>
+</div>
+
+<div class="media-element">
+  <a href="images/media/visuels-pp_media-012.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs012.jpg" alt="media-holder"></a>
+</div>
+
+<div class="media-element">
+  <a href="images/media/visuels-pp_media-013.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs013.jpg" alt="media-holder"></a>
+</div>
+
+<div class="media-element">
+  <a href="images/media/visuels-pp_media-014.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs014.jpg" alt="media-holder"></a>
 </div>
