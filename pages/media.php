dee1cf6b8a04f2a75e0a353403292c25bc1fb256
daniel rodriguez <maieutiquer@gmail.com>
2014-09-19 15:39:40 +0300

    open media images in a new tab

--- pages/media.php
+++ pages/media.php
@@ -6,93 +6,93 @@
 <img class="vernissage-plume-et-pinceau" src="images/media/vernissage-plume-et-pinceau.png" alt="vernissage plume et pinceau">
 
 <div class="media-element">
-  <a href="images/media/kc14_031_00.png"><img class="media-holder" src="images/media/thumbs_kc14_031_00.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/kc14_031_00.png"><img class="media-holder" src="images/media/thumbs_kc14_031_00.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/kc14_031_01.jpg"><img class="media-holder" src="images/media/thumbs_kc14_031_01.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/kc14_031_01.jpg"><img class="media-holder" src="images/media/thumbs_kc14_031_01.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/kc14_031_02.jpg"><img class="media-holder" src="images/media/thumbs_kc14_031_02.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/kc14_031_02.jpg"><img class="media-holder" src="images/media/thumbs_kc14_031_02.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/kc14_031_03.jpg"><img class="media-holder" src="images/media/thumbs_kc14_031_03.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/kc14_031_03.jpg"><img class="media-holder" src="images/media/thumbs_kc14_031_03.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/kc14_031_04.jpg"><img class="media-holder" src="images/media/thumbs_kc14_031_04.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/kc14_031_04.jpg"><img class="media-holder" src="images/media/thumbs_kc14_031_04.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/kc14_031_05.jpg"><img class="media-holder" src="images/media/thumbs_kc14_031_05.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/kc14_031_05.jpg"><img class="media-holder" src="images/media/thumbs_kc14_031_05.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/kc14_031_06.jpg"><img class="media-holder" src="images/media/thumbs_kc14_031_06.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/kc14_031_06.jpg"><img class="media-holder" src="images/media/thumbs_kc14_031_06.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/kc14_031_07.jpg"><img class="media-holder" src="images/media/thumbs_kc14_031_07.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/kc14_031_07.jpg"><img class="media-holder" src="images/media/thumbs_kc14_031_07.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/kc14_031_08.jpg"><img class="media-holder" src="images/media/thumbs_kc14_031_08.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/kc14_031_08.jpg"><img class="media-holder" src="images/media/thumbs_kc14_031_08.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/visuels-pp_media-01.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs01.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/visuels-pp_media-01.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs01.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/visuels-pp_media-02.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs02.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/visuels-pp_media-02.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs02.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/visuels-pp_media-03.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs03.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/visuels-pp_media-03.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs03.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/visuels-pp_media-04.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs04.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/visuels-pp_media-04.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs04.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/visuels-pp_media-05.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs05.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/visuels-pp_media-05.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs05.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/visuels-pp_media-06.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs06.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/visuels-pp_media-06.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs06.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/visuels-pp_media-07.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs07.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/visuels-pp_media-07.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs07.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/visuels-pp_media-08.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs08.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/visuels-pp_media-08.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs08.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/visuels-pp_media-09.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs09.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/visuels-pp_media-09.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs09.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/visuels-pp_media-010.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs010.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/visuels-pp_media-010.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs010.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/visuels-pp_media-011.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs011.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/visuels-pp_media-011.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs011.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/visuels-pp_media-012.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs012.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/visuels-pp_media-012.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs012.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/visuels-pp_media-013.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs013.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/visuels-pp_media-013.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs013.jpg" alt="media-holder"></a>
 </div>
 
 <div class="media-element">
-  <a href="images/media/visuels-pp_media-014.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs014.jpg" alt="media-holder"></a>
+  <a target="_blank" href="images/media/visuels-pp_media-014.jpg"><img class="media-holder" src="images/media/visuels-pp_media-thumbs014.jpg" alt="media-holder"></a>
 </div>
